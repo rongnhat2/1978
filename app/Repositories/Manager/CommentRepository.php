f57a841b6e11a225fbbea30179cc52b830986324
Nguyen Long <rongnhat2@gmail.com>
2023-02-13 07:19:22 +0700

    upload data

---
+++ app/Repositories/Manager/CommentRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories\Manager;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Repositories\BaseRepository;
+use App\Repositories\RepositoryInterface;
+use Session;
+use Hash;
+use DB;
+
+class CommentRepository extends BaseRepository implements RepositoryInterface
+{
+    protected $model;
+
+    public function __construct(Model $model){
+        $this->model = $model;
+    }
+
+    public function get_all($id){ 
+        return DB::table('comment')->where("id", "=", $id)->get(); 
+    } 
+    public function get_one($id){
+        $sql = "SELECT * FROM category WHERE id = ".$id;
+        return DB::select($sql);
+    }
+    
+}
